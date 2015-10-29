433d69cbe59121767d9e6566a4e4de34b3faf7cd
Daniel Newton <dantheman.uah@gmail.com>
2015-10-28 21:31:17 -0500

    Added C# test program. Test program calls Panel::ShowMessage() methods from Panel_QA.dll.

--- Panel_QA/Panel_QA/Panel_QA.h
+++ Panel_QA/Panel_QA/Panel_QA.h
@@ -1,15 +1,21 @@
 // Panel_QA.h
 using namespace std;
+using namespace System::Windows::Forms;
+using namespace System;
 
 #pragma once
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
 
-class Panel
+namespace Panels
 {
-public:
-	Panel();
-	~Panel();
-	void PrintMessage(string message);
-};
+	public ref class Panel
+	{
+	private:
+	public:
+		Panel();
+		~Panel();
+		void ShowMessage(String^ message);
+	};
+}
